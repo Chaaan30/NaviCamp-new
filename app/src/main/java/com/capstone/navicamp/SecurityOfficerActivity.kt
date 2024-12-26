78b2a7682c14fb00d00703b88ef9879f39b5033c
Chaaan30 <cristian013003@gmail.com>
2024-12-26 13:14:24 +0800

    added activities

---
+++ app/src/main/java/com/capstone/navicamp/SecurityOfficerActivity.kt
@@ -0,0 +1,11 @@
+package com.capstone.navicamp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SecurityOfficerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_security_officer)
+    }
+}
